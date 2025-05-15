cd3f000392f7287c1143548e05e8f584e29d6798
David Vazquez <dave_u@outlook.com>
2025-05-14 21:14:48 -0600

    add ContadorEstado state and pagina_info component for state management

--- proyecto_pa/proyecto_pa.py
+++ proyecto_pa/proyecto_pa.py
@@ -4,6 +4,18 @@ import reflex as rx
 
 from rxconfig import config
 
+class ContadorEstado(rx.State):
+    """State for the Contador component."""
+
+    count: int = 0
+
+    @rx.event
+    def increment(self):
+        self.count += 1
+    @rx.event
+    def decrement(self):
+        self.count -= 1
+
 
 class State(rx.State):
     """The app state."""
@@ -34,6 +46,20 @@ def index() -> rx.Component:
         rx.logo(),
     )
 
+def pagina_info() -> rx.Component:
+    
+    return rx.container(
+        rx.center(
+            rx.vstack(
+                rx.text('Cambio de estado', size="8"),
+                rx.text(ContadorEstado.count , size="9", justify="center"),
+                rx.button('Incrementar' , on_click=ContadorEstado.increment),
+                rx.button('Decrementar' , on_click=ContadorEstado.decrement),
+            )
+        )
+    )
+
 
 app = rx.App()
 app.add_page(index)
+app.add_page(pagina_info, route="/info")
